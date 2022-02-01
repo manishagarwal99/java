82b57b0cc8637d5cb310ee6f262431200e063bfb
MANISH AGARWAL <agarwal29manish@gmail.com>
2022-02-01 13:33:48 +0530

    Update RnaTranscription.java

--- exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
+++ exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,26 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String rnaStrand = "";
+        for(int i=0;i<dnaStrand.length();i++){
+            switch(dnaStrand.charAt(i)){
+                case 'G':
+                    rnaStrand+='C';
+                    break;
+                case 'C':
+                    rnaStrand+='G';
+                    break;
+                case 'T':
+                    rnaStrand+='A';
+                    break;
+                case 'A':
+                    rnaStrand+='U';
+                    break;
+                default:
+                    break;
+            }
+        }
+        return rnaStrand;
     }
 
 }
